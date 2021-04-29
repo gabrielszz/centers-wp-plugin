75d7d7f34be5171091ffe5628cf83e901cf49c8f
Vinicius de Andrade <viniciusdeandrade@gmail.com>
2021-04-29 18:06:16 -0300

    Correção na lib de paginação. #20

--- lib/Paginator.php
+++ lib/Paginator.php
@@ -19,16 +19,12 @@ class Paginator{
         $pag['total'] = $this->total;
         $pag['total_formatted'] = number_format($pag['total'], 0, ',', '.');
         $pag['start'] = $this->start;
-        $pag['total_pages'] = (($pag['total']/$this->count) % 10 == 0) ? (int)($pag['total']/$this->count) : (int)($pag['total']/$this->count+1);
+        $pag['total_pages'] = ($pag['total'] % $this->count == 0) ? (int)($pag['total']/$this->count) : (int)($pag['total']/$this->count+1);
         $pag['count'] = $this->count;
-
-
-
 
         $range_min = (($this->current_page-5) > 0) ? $this->current_page-5 : 1;
         $range_max = (($range_min+7) > $pag['total_pages']) ? $pag['total_pages'] : $range_min+7;
         $pag['pages'] = range($range_min, $range_max);
-
 
         $this->output  = '<nav aria-label="Page navigation example">';
         $this->output .= '<ul class="pagination justify-content-center">';
