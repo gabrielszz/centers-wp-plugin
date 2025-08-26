05f57d36e6646a5e192f7676f0fd811fb69ffa6c
Do Carmo <docarmgab@paho.org>
2025-08-26 12:26:57 -0300

    Remoção de links no rodapé
    Removido links do rodapé, conforme solicitado no issue abaixo:
    
    https://github.com/issues/assigned?issue=bireme%7Cwordpress-themes%7C248
    
    Para manter a harmonia foi centralizado o restante do conteúdo.

--- template/footer.php
+++ template/footer.php
@@ -1,13 +1,14 @@
     <footer id="footer">
     	<div class="container">
     		<div class="row">
+				<!--
     			<div class="col-md-5">
     				<small>
     					<a href="https://politicas.bireme.org/terminos/<?php echo $lang ?>" target="_blank"><?php _e('Terms and conditions of use', 'cc'); ?></a> |
     					<a href="https://politicas.bireme.org/privacidad/<?php echo $lang ?>" target="_blank"><?php _e('Privacy Policy', 'cc'); ?></a>
     				</small>
-    			</div>
-    			<div class="col-md-7 text-right" id="logoOPAS">
+    			</div>-->
+    			<div class="col-md-12 text-center" id="logoOPAS">
     				<img src="https://logos.bireme.org/img/<?php echo $lang ?>/h_bir_color.svg" alt="" class="img-fluid imgBlack">
     			</div>
     		</div>
